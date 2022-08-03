765e8add4241c8e6f4e934b5908db826bcac6070
nsengiyumvawilberforce@gmail.com <nsengiyumvawilberforce@gmail.com>
2022-08-03 19:54:04 +0300

    audio api

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="sidebar bg-success" data-color="green" data-background-color="green" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
+<div class="sidebar bg-danger" data-color="green" data-background-color="red" data-image="{{ asset('material') }}/img/sidebar-1.jpg">
   <!--
       Tip 1: You can change the color of the sidebar using: data-color="purple | azure | green | orange | danger"
 
